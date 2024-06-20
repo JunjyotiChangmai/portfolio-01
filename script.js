f6ffa97f5be68ebdc1dd806e50268198d5cc23af
Junjyoti Changmai <junjyotichangmai12@gmail.com>
2024-06-20 22:10:32 +0530

    full update

--- script.js
+++ script.js
@@ -1,9 +1,23 @@
-const menu = document.getElementById("menu-Icon");
-menu.addEventListener("click", function() {
-    document.getElementById("side-bar").style.display = "block";
-});
+window.addEventListener("scroll", function () {
+    const nav = document.getElementsByClassName("container");
+    const navLi = document.getElementsByTagName("a");
 
-const cross = document.getElementById("cross-icon");
-cross.addEventListener("click", function() {
-    document.getElementById("side-bar").style.display = "none";
+    if (window.scrollY > 30) {
+        nav[0].style.backgroundColor = "white";
+        nav[0].style.color = "black";
+        navLi[0].style.color = "black";
+        navLi[1].style.color = "black";
+        navLi[2].style.color = "black";
+        navLi[3].style.color = "black";
+        navLi[4].style.color = "black";
+    }
+    else {
+        nav[0].style.backgroundColor = "#3056D3";
+        nav[0].style.color = "white";
+        navLi[0].style.color = "white";
+        navLi[1].style.color = "white";
+        navLi[2].style.color = "white";
+        navLi[3].style.color = "white";
+        navLi[4].style.color = "white";
+    }
 });
